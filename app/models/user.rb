188f810d9d5d4661a0b2eaea9f5b409e6d93f059
Laguna1 <lagunazoomarket@gmail.com>
2021-10-30 14:35:05 +0300

    Add associations friendship-user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friend_sent, class_name => 'Friendship', foreign_key: 'sent_by_id'
+  has_many :friend_request, class_name => 'Friendship', foreign_key: 'sent_to_id'
 
   validates :fname, presence: true, length: { in: 3..15 }
   validates :lname, presence: true, length: { in: 3..15 }
