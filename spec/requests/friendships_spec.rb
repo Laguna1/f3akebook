a29d277cb4af8e65b712a19c248f5e2b7b920e7d
Laguna1 <lagunazoomarket@gmail.com>
2021-10-30 14:58:46 +0300

    Add ability to user to accept and decline friendship requests

--- spec/requests/friendships_spec.rb
+++ spec/requests/friendships_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Friendships", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/friendships/create"
+RSpec.describe 'Friendships', type: :request do
+  describe 'GET /create' do
+    it 'returns http success' do
+      get '/friendships/create'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
