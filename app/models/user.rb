09924fa2645b844d9658b9bc95f9a47b83eceac5
Laguna1 <lagunazoomarket@gmail.com>
2021-11-01 20:08:59 +0200

    Modify comments-, friendships-, likes- controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friend_sent, class_name: 'Friendship', foreign_key: 'sent_by_id', dependent: :destroy
   has_many :friend_request, class_name: 'Friendship', foreign_key: 'sent_to_id', dependent: :destroy
-
+  has_many :notifications
+  
   validates :fname, presence: true, length: { in: 3..15 }
   validates :lname, presence: true, length: { in: 3..15 }
 end
