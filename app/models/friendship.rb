1a9c7d330dc129c3e0378ad12b159de0ed600e24
Laguna1 <lagunazoomarket@gmail.com>
2021-11-05 01:36:46 +0200

    Change associations for friends in user model from object based to database based

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,4 +4,5 @@ class Friendship < ApplicationRecord
   belongs_to :sent_to, class_name: 'User', foreign_key: 'sent_to_id'
   belongs_to :sent_by, class_name: 'User', foreign_key: 'sent_by_id'
   scope :friends, -> { where('status =?', true) }
+  scope :not_friends, -> { where('status =?', false) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,29 +14,40 @@ class User < ApplicationRecord
   has_many :friend_sent, class_name: 'Friendship', foreign_key: 'sent_by_id', inverse_of: 'sent_by', dependent: :destroy
   has_many :friend_request, class_name: 'Friendship', foreign_key: 'sent_to_id', inverse_of: 'sent_to',
                             dependent: :destroy
+  has_many :friends, -> { merge(Friendship.friends) }, through: :friend_sent, source: :sent_to
+  has_many :pending_requests, lambda {
+                                merge(Friendship.not_friends)
+                              }, through: :friend_sent, source: :sent_to
+  has_many :recieved_requests, lambda {
+                                 merge(Friendship.not_friends)
+                               }, through: :friend_request, source: :sent_by
   has_many :notifications, dependent: :destroy
 
   validates :fname, presence: true, length: { in: 3..15 }
   validates :lname, presence: true, length: { in: 3..15 }
 
+  def full_name
+    "#{fname} #{lname}"
+  end
+
   # Searches Friendship database and returns array, 'friends',
   # which contains records of all mutual friendships for current_user
-  def friends
-    my_friends = Friendship.friends.where('sent_by_id =?', id)
-    friend_ids = []
-    my_friends.each do |f|
-      friend_ids << f.sent_to_id if f.sent_to_id != id
-      friend_ids << f.sent_by_id if f.sent_by_id != id
-    end
+  # def friends
+  #   my_friends = Friendship.friends.where('sent_by_id =?', id)
+  #   friend_ids = []
+  #   my_friends.each do |f|
+  #     friend_ids << f.sent_to_id if f.sent_to_id != id
+  #     friend_ids << f.sent_by_id if f.sent_by_id != id
+  #   end
 
-    # Adds the User record from each friend id retrieved into friends array
-    friends = []
-    friend_ids.each do |i|
-      friends << User.find(i)
-    end
+  # Adds the User record from each friend id retrieved into friends array
+  #   friends = []
+  #   friend_ids.each do |i|
+  #     friends << User.find(i)
+  #   end
 
-    friends
-  end
+  #   friends
+  # end
 
   # Returns all posts from this user's friends and self
   def friends_and_own_posts
@@ -53,22 +64,22 @@ class User < ApplicationRecord
   end
 
   # Returns all users that current user has sent a friend request to but hasn't accepted or declined
-  def pending_requests
-    friends_requested = []
-    friend_sent.each do |r|
-      friends_requested << r.sent_to if r.status == false
-    end
+  # def pending_requests
+  #   friends_requested = []
+  #   friend_sent.each do |r|
+  #     friends_requested << r.sent_to if r.status == false
+  #   end
 
-    friends_requested
-  end
+  #   friends_requested
+  # end
 
   # Returns all users that current user has recieved a friend request from but hasn't accepted or declined
-  def recieved_requests
-    friend_requests = []
-    friend_request.each do |r|
-      friend_requests << r.sent_by if r.status == false
-    end
+  # def recieved_requests
+  #   friend_requests = []
+  #   friend_request.each do |r|
+  #     friend_requests << r.sent_by if r.status == false
+  #   end
 
-    friend_requests
-  end
+  #   friend_requests
+  # end
 end
